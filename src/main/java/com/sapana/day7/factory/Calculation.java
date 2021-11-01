6d3edf3a19ea2b12517170e532a2459fa4de3e81
sapanarimal12 <rimalsapanaaa55@gmail.com>
2021-11-01 14:41:49 +0545

    dar7

---
+++ src/main/java/com/sapana/day7/factory/Calculation.java
@@ -0,0 +1,6 @@
+package com.sapana.day7.factory;
+
+public  abstract class Calculation {
+
+    public   Double calculation;
+}
---
+++ src/main/java/com/sapana/day7/factory/FactoryMain.java
@@ -0,0 +1,5 @@
+package com.sapana.day7.factory;
+
+public class FactoryMain {
+
+}
